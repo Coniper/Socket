169cfb12e721b1079bdec9f795fea473fdc6e98e
Coniper <Coniper@outlook.com>
2021-08-21 17:07:06 +0800

    UDP
    UDP简易服务器、客户端

---
+++ UDP/2_udp_client.c
@@ -0,0 +1,67 @@
+/*******************************************************
+	> File Name: 2_udp_client.c
+	> Author:Coniper
+	> Describe: 
+	> Created Time: 2021年08月20日
+ *******************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define BUFSZ 128
+#define ERR_LOG(LOG) do{perror(LOG);break;}while(0)
+
+int main(int argc, char *argv[])
+{
+    //创建套接字
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(-1 == sockfd)
+    {
+        ERR_LOG("socket");
+    }
+
+    //填写三元组信息
+    struct sockaddr_in seraddr;
+    seraddr.sin_family      = AF_INET;
+    seraddr.sin_port        = htons(8888);
+    seraddr.sin_addr.s_addr = inet_addr("192.168.164.128");
+
+    printf("udp client init success\n");
+
+    char buf[BUFSZ] = "";
+
+    //交互
+    while(1)
+    {
+        printf("input: ");
+        fgets(buf, sizeof(buf), stdin);
+
+        if(-1 == sendto(sockfd, buf, sizeof(buf), 0, \
+                    (struct sockaddr *)&seraddr, sizeof(seraddr)))
+        {
+            ERR_LOG("sendto");
+        }
+        
+        bzero(buf, sizeof(buf));
+
+        if(-1 == recvfrom(sockfd, buf, sizeof(buf), 0, NULL, NULL))
+        {
+            ERR_LOG("recvfrom");
+        }
+        else
+        {
+            printf("recv: %s\n", buf);
+        }
+    }
+
+    close(sockfd);
+
+    return 0;
+}
+
